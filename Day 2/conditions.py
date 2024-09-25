d9d5d2cac2cba6d9158eeb288ca082e9d790f922
Nishanth18 <nishanthhs8@gmail.com>
2024-09-25 14:09:15 +0530

    Create conditions.py

---
+++ Day 2/conditions.py
@@ -0,0 +1,15 @@
+x = 10
+y = 20 
+
+# example 1
+vvitstudent = "Nishnath"
+if(vvitstudent == 'Nishnath'):
+    print('Entry to college')
+else:
+    print('No entry to college')
+
+# exam 2
+if (x == 20):
+    print("x is less than y")
+else:
+    print("x is not less than y")
