be10cce5e178869cd80f95064970bfa866706ad2
Nishanth18 <nishanthhs8@gmail.com>
2024-09-25 14:09:25 +0530

    Create Dictionary.py

---
+++ Day 2/Dictionary.py
@@ -0,0 +1,31 @@
+students = {'name':'nishnath', 'age':21, 'branch':'cse'}
+print(students)
+
+students['college'] = 'vvit'
+print(students)
+
+# students['marks'] = [34,50,65,89,98]
+# print(students)
+
+# #get the element
+# print(students.get('age'))
+
+# #add
+# students['gender'] = 'male'
+
+# #update
+# students['age'] = 23
+# print(students)
+
+# #items
+# students.items()
+
+# #keys of the dict
+# print(students.keys())
+
+# #values of the dict
+# print(students.values())
+
+# #pop function
+# print(students.pop('age'))
+
