a519fb19d9706fc0747ec5c18f32b00fdac919ea
Nishanth18 <nishanthhs8@gmail.com>
2024-09-25 14:31:06 +0530

    Create operatordemo.py

---
+++ Day 2/operatordemo.py
@@ -0,0 +1,20 @@
+v1 = 12
+v2 = 13
+res = v1 + v2
+print(res)
+
+v1 = 11
+v2 = 14
+res1 = v2 - v1
+print(res1)
+
+v1 = 10
+v2 = 10
+res2 = v2 * v1
+print(res2)
+
+v1 = 10
+v2 = 32
+res2 = v2 / v1
+print(res2)
+
