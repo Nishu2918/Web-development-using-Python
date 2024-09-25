ee181e11cfc9d956c9ebacac7d59fc7faa1e5971
Nishanth18 <nishanthhs8@gmail.com>
2024-09-25 14:30:54 +0530

    Create listdemo.py

---
+++ Day 2/listdemo.py
@@ -0,0 +1,16 @@
+# Example 1
+
+fruits = ["pear","melon","orange"]
+print(fruits)
+
+fruits.insert(2, 'banana')
+print(fruits)
+
+students = ["Nishanth","Keerthi","Sumit","Shrreram"]
+print(students)
+
+fruits.insert(0, 'Apple')
+print(fruits)
+
+print(fruits[3])
+print(type(fruits))
