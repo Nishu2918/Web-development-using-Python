ba90ce8c5d3ec56b3016620d3dfaf5f50b7a0ec5
Nishanth18 <nishanthhs8@gmail.com>
2024-09-25 14:09:43 +0530

    Create global and local scope.py

---
+++ Day 2/global and local scope.py
@@ -0,0 +1,8 @@
+icecream = "vanilla"
+def foods():
+    vegetable = "potato"
+    fruit = "Lichi"
+    print(vegetable + " is a local variable value.")
+    print(icecream + " is a global variable value.")
+    print(fruit + " is a local variable value.")
+foods()
