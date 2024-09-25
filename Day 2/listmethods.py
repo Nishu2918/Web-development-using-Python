a2499b038060ed886ce4b19a128967a2e2a13fd2
Nishanth18 <nishanthhs8@gmail.com>
2024-09-25 14:30:58 +0530

    Create listmethods.py

---
+++ Day 2/listmethods.py
@@ -0,0 +1,32 @@
+# extending a list
+
+prices_a = [10, 20, 30]
+prices_b = [40, 50, 60]
+
+prices_a.extend(prices_b)
+print(prices_a)
+
+
+# count the number of occurrences
+
+stocks = ["Nishanth","Keerthi","Sumit","Shrreram"]
+print(stocks.count('Sumit'))
+
+
+
+# # reverse the order of the values
+
+
+sectors = ["ULVR", "BMW", "aapl", "AAPL"]
+sectors.reverse()
+print(sectors)
+
+
+
+
+# # # # list
+# # # []
+# # # mutable
+# # # deleting an element
+# # ordered
+# #dublicate
