e48eb07bd51b32b50d7992ec21b35c3ef3de5ad6
Nishanth18 <nishanthhs8@gmail.com>
2024-09-26 10:56:49 +0530

    Create tempCodeRunnerFile.py

---
+++ Day 3 (OOPS concept in python)/tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+class myClass():
+    def method1(self):
+        print("vviet")
+
+    def method2(Self,someString):
+        print("Project training:"+someString)
+
+
+## excercise the class method
+
+c = myClass()
+c.method1()
+c.method2("Training is fun")
