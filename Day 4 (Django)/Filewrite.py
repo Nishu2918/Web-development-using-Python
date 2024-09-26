b4767036055e52db47b509b3692d1457f93cd7e0
Nishanth18 <nishanthhs8@gmail.com>
2024-09-26 10:57:30 +0530

    Create Filewrite.py

---
+++ Day 4 (Django)/Filewrite.py
@@ -0,0 +1,9 @@
+# my_file = open("my_file2.txt","w")
+# my_file.write("Welcome to my class ")
+# my_file.close()
+
+
+
+f = open("Shree.txt","wt")
+f.write("this first line\n my name is shrre")
+f.close()
