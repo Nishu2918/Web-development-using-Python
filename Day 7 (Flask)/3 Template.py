2798c0d5e6827bf6764abfc0f1bcd9698818a50c
Nishanth18 <nishanthhs8@gmail.com>
2024-09-26 10:58:40 +0530

    Create 3 Template.py

---
+++ Day 7 (Flask)/3 Template.py
@@ -0,0 +1,19 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    greet = '<h1>Hello, Students of VVIET College</h1>'
+    link = '<p><a href="user/Nishanth">Click me</a></h1>'
+    return greet + link
+
+@app.route('/user/<name>')
+def user(name):
+    personal = f'<h1>Hello, {name}!</h1>'
+    instruc = f'<p>Mr {name} Check the name in the <strong>browser address bar</strong> and reload the page.</p>'
+    return personal + instruc
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
